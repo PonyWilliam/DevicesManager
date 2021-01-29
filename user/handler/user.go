83cb2c63bdbc24e39fe8e2c780b23becb4f8fb55
william <tjw@dadiqq.cn>
2021-01-29 12:51:37 +0800

    转移仓库

---
+++ user/handler/user.go
@@ -0,0 +1,53 @@
+package handler
+
+import (
+	"context"
+	"devices/user/domain/model"
+	"devices/user/domain/service"
+	users "devices/user/proto"
+)
+
+//对外暴露的服务都在这里编写。
+type User struct{
+	UserDataService service.IUserDataService
+}
+//注册
+func(u *User)Register(ctx context.Context,request *users.UserRegisterRequest ,response *users.UserRegisterResponse)error{
+	userRegister := &model.User{
+		UserName: request.UserName,
+		FirstName: request.FirstName,
+		HashPassword: request.Pwd,
+	}
+	_,err := u.UserDataService.AddUser(userRegister)
+	if err!=nil{
+		return err
+	}
+	response.Message = "添加成功"
+	return nil
+}
+//登陆
+func(u *User)Login(ctx context.Context,request *users.UserLoginRequest,response *users.UserLoginResponse) error{
+	isOk,err := u.UserDataService.CheckPwd(request.UserName,request.Pwd)
+	if err != nil{
+		return err
+	}
+	response.IsSuccess = isOk
+	return nil
+}
+func(u *User)GetUserInfo(ctx context.Context,request *users.UserInfoRequest,response *users.UserInfoResponse) error{
+	userInfo,err := u.UserDataService.FindUserByID(request.UserId)
+	if err != nil{
+		return err
+	}
+	response = GetUserForResponse(userInfo)
+	return nil
+}
+
+//类型转化
+func GetUserForResponse(userModel *model.User) *users.UserInfoResponse{
+	response := &users.UserInfoResponse{}
+	response.UserName = userModel.UserName
+	response.FirstName = userModel.FirstName
+	response.UserId = userModel.ID
+	return response
+}
---
+++ user/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"devices/common"
+	"devices/user/domain/repository"
+	service2 "devices/user/domain/service"
+	"devices/user/handler"
+	users "devices/user/proto"
+	"github.com/jinzhu/gorm"
+	"github.com/micro/go-micro/v2"
+	"github.com/micro/go-micro/v2/registry"
+	"github.com/micro/go-micro/v2/util/log"
+	"github.com/micro/go-plugins/registry/consul/v2"
+	"strconv"
+	"time"
+)
+
+func main() {
+	//配置中心
+	consulConfig,err := common.GetConsualConfig("127.0.0.1",8500,"/micro/config")
+	if err != nil{
+		//记录日志
+		log.Error(err)
+	}
+	//注册中心
+	consulRegistry := consul.NewRegistry(func(options *registry.Options){
+		options.Addrs = []string{"127.0.0.1"}
+		options.Timeout = 10 * time.Second
+	})
+	//NEW service
+	srv := micro.NewService(
+		micro.Name("service.user"),
+		micro.Version("latest"),
+		//这里设置地址和需要暴露的端口
+		micro.Address("127.0.0.1:8081"),
+		//添加consul 作为注册中心
+		micro.Registry(consulRegistry),
+		)
+	//路径中不用代前缀
+	mysqlInfo := common.GetMysqlFromConsul(consulConfig,"mysql")
+	//创建数据库
+	db,err := gorm.Open("mysql",
+		mysqlInfo.User+":"+mysqlInfo.Pwd+"@tcp("+mysqlInfo.Host + ":"+ strconv.FormatInt(mysqlInfo.Port,10) +")/"+mysqlInfo.DataBase+"?charset=utf8&parseTime=True&loc=Local",
+		)
+	if err!=nil{
+		log.Error(err)
+	}
+	defer db.Close()
+	//执行完所有后关闭数据库
+	db.SingularTable(true)
+
+	srv.Init()
+
+
+
+	rp:=repository.NewUserRepository(db)
+	rp.I()
+
+	userDataservice := service2.NewUserDataService(repository.NewUserRepository(db))
+	//创建实例
+	err = users.RegisterUserHandler(srv.Server(),&handler.User{
+		UserDataService: userDataservice,
+	})
+	//绑定实例
+	if err!=nil{
+		log.Fatal(err)
+	}
+	if err:=srv.Run();err!=nil{
+		log.Fatal(err)
+	}
+}
